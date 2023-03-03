68971b16ceb0c1266d42fbc0d395d1087c8cb5e6
Rodrigo Orozco <orozcorodri8@gmail.com>
2023-03-03 19:25:19 -0300

    modal dialog with angular material added and binded to the button in toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { PeopleTableComponent } from '../people-table';
 
 @Component({
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [MatToolbarModule, MatIconModule, MatDialogModule],
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  constructor(private dialog: MatDialog) {}
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ): void {
+    this.dialog.open(PeopleTableComponent, {
+      // width: '50%',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+}
